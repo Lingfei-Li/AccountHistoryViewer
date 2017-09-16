a16bdd7f519b13b3067fe8c1a5d431f85b5e14de
Lingfei Li <cslilingfei@outlook.com>
2017-09-15 22:29:23 -0700

    Finish daily balance calculating stream processor

--- api/history.js
+++ api/history.js
@@ -5,8 +5,11 @@ const AWS = require('aws-sdk');
 const moment = require('moment');
 const BloomFilter = require('bloom-filter');
 
-const BALANCE_TABLE = "account-balance";
+const BALANCE_TABLE = "account-daily-balance";
 const HISTORY_TABLE = "account-history-service";
+
+const defaultDailyTransactionAmount = 30;
+const defaultFalsePositiveRate = 0.001;
 
 AWS.config.setPromisesDependency(require('bluebird'));
 
@@ -70,34 +73,29 @@ exports.listBetweenDates = (event, context, callback) => {
             ':end_date_sec': endDateSec,
         }
     };
-    const onScan = (err, data) => {
-        if(err) {
-            const response = {
-                statusCode: 400,
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify({
-                    message: "Path parameters missing or they cannot be converted to integer",
-                    error: err
-                }),
-            };
-            return callback(null, response);
-        }
-        else {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify({
-                    history: data.Items
-                }),
-            };
-            return callback(null, response);
-        }
-    };
-    dynamodb.scan(params, onScan);
+    dynamodb.scan(params, onScan).promise().then(function(data) {
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                history: data.Items
+            }),
+        };
+        return callback(null, response);
+    }).then(function(err) {
+        const response = {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                error: err
+            }),
+        };
+        return callback(null, response);
+    });
 };
 
 exports.calculateDailyBalance = (event, context, callback) => {
@@ -105,8 +103,8 @@ exports.calculateDailyBalance = (event, context, callback) => {
         console.log('Stream record: ', JSON.stringify(record, null, 2));
         if (record.eventName === 'INSERT') {
             let amount = record.dynamodb.NewImage.amount.N;
-            let transaction_date_sec = record.dynamodb.NewImage.transaction_date_sec.N;
-            const params = {
+            let transaction_date_sec = parseInt(record.dynamodb.NewImage.transaction_date_sec.N);
+            let params = {
                 TableName: BALANCE_TABLE,
                 ProjectionExpression: 'date_sec, sequence_number, bloom_filter, balance',
                 KeyConditionExpression: 'date_sec = :transaction_date_sec',
@@ -116,9 +114,64 @@ exports.calculateDailyBalance = (event, context, callback) => {
                 Limit: 1,
                 ScanIndexForward: false
             };
+            console.log("Start query");
             dynamodb.query(params).promise().then(function(data) {
-                console.log(data.Items);
+                if(data.Items.length === 0) {
+                    let bloom_filter = BloomFilter.create(defaultDailyTransactionAmount, defaultFalsePositiveRate);
+                    bloom_filter.insert(transaction_date_sec);
+                    let params = {
+                        TableName: BALANCE_TABLE,
+                        Item: {
+                            date_sec: transaction_date_sec,
+                            version: 1,
+                            amount: amount,
+                            bloom_filter: JSON.stringify(bloom_filter.toObject())
+                        }
+                    };
+                    dynamodb.put(params).promise().then(function(data) {
+                        console.log("Data successfully put in table");
+                        console.log(JSON.stringify(data, null, 2));
+                    }).catch(function(err) {
+                        console.error("Failed to put the data: ");
+                        console.error(JSON.stringify(params.Item, null, 2));
+                        console.error("error:\n", err);
+                    });
+                }
+                else {
+                    if(data.Items.length !== 1) {
+                        console.error("Daily balance retrieved from DynamoDB has more than 1 value");
+                    }
+                    let balance_data = data.Items[0];
+                    let version = balance_data.version;
+                    let balance_amount = balance_data.amount;
+                    let bloom_filter_str = balance_data.bloom_filter;
+                    let bloom_filter = BloomFilter(JSON.parse(bloom_filter_str));
+                    if(bloom_filter.contains(transaction_date_sec)) {
+                        console.warn("Bloom Filter already contains the transaction: ");
+                        console.warn(params.Item);
+                        return;
+                    }
+                    bloom_filter.insert(transaction_date_sec);
+                    let params = {
+                        TableName: BALANCE_TABLE,
+                        Item: {
+                            date_sec: transaction_date_sec,
+                            version: version + 1,
+                            amount: balance_amount + amount,
+                            bloom_filter: JSON.stringify(bloom_filter.toObject())
+                        }
+                    };
+                    dynamodb.put(params).promise().then(function(data) {
+                        console.log("Data successfully put in table: ");
+                        console.log(JSON.stringify(data, null, 2));
+                    }).catch(function(err) {
+                        console.error("Failed to put the data: ");
+                        console.error(JSON.stringify(params.Item, null, 2));
+                        console.error("error:\n", err);
+                    });
+                }
             }).catch(function(err) {
+                console.error("Got an error querying daily balance");
                 console.error(err);
             });
         }
